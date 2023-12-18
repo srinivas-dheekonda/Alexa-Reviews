8d3e5a6a3f711fd2b07d9fe87806daac9224add7
Srinivasara_dheekonda <vasuvasu.srinu1996@gmail.com>
2023-12-18 21:30:00 +0530

    requirements & installation done

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ ="0.0.0"
+
+REPO_NAME = "end-to-end-alexa_reviews"
+AUTHOR_USER_NAME = "srinivas-dheekonda"
+SRC_REPO = "Alexareviews"
+AUTHOR_EMAIL="vasuvasu.srinu1996@gmail.com"
+
+
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "alexa_reviews":f"http://github.com/{AUTHOR_USER_NAME}{REPO_NAME}/issues",
+
+    },
+
+    package_dir = {"":"src"},
+    packages=setuptools.find_packages(where="src"),
+)
